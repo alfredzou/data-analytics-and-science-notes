4878daa1d8b2aca0d7228ef7344cbc80a5e898e9
Alfred Zou <alfredzou@outlook.com>
2020-06-29 16:35:19 +1000

    commit test

---
+++ Scripts/vscode.py
@@ -0,0 +1,17 @@
+import math
+import sys
+from os import rename
+
+import requests
+
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    test = "Test"
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+r = requests.get("https://coreyms.com")
+print(r.status_code)
